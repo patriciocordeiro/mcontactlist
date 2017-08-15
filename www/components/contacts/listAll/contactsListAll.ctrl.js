c4f93e202909dd6f494d2b2b5162fb66ae99e6a9
patriciocordeiro <paponcio6@gmail.com>
2017-08-15 17:47:23 -0300

    adicionando um novo contato

--- www/components/contacts/listAll/contactsListAll.ctrl.js
+++ www/components/contacts/listAll/contactsListAll.ctrl.js
@@ -2,10 +2,10 @@
     'use strict';
 
 
-    angular.module('mcontactListApp').controller('ContactListAllCtrl', ['ContactDataSrcv', ContactListAllCtrl]);
+    angular.module('mcontactListApp').controller('ContactListAllCtrl', ['ContactDataSrcv', '$mdDialog', ContactListAllCtrl]);
 
 
-    function ContactListAllCtrl(ContactDataSrcv) {
+    function ContactListAllCtrl(ContactDataSrcv, $mdDialog) {
 
         /*bind this controller to a vm*/
         var vm = this;
@@ -13,6 +13,48 @@
         //get contacts from service
         vm.contacts = ContactDataSrcv.contacts
         console.log(ContactDataSrcv.contacts);
+
+
+        /************ Dialog for add new contact *************/
+
+        vm.openAddContactDialg = function (ev) {
+
+            vm.contactToEdit = angular.copy(vm.selectedContact);
+            $mdDialog.show({
+                    controller: ('DialogCtrl', function ($scope) {
+
+                        $scope.newContact = {};
+                        $scope.cancelDialg = function () {
+                            $scope.newContact = {};
+                            $mdDialog.cancel();
+                        };
+
+                        $scope.confirmDialg = function (answer) {
+                            $mdDialog.hide(answer);
+                        };
+
+                    }),
+                    templateUrl: 'components/contacts/listAll/contactAdd.dialg.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: false,
+                    locals: {
+                        contact: vm.contactToEdit
+                    }
+                })
+                .then(function (newContact) {
+                    /*Dialog confirmed*/
+                    console.log(newContact);
+                    //update contact on service
+                    vm.contacts.push(newContact);
+
+                }, function () {
+
+                    /*Dialog canceled*/
+                });
+        };
+
+
 
     }
 
