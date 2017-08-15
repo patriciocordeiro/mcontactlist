4321c0818455d5a5059cabeefb163ddc94fe8203
patriciocordeiro <paponcio6@gmail.com>
2017-08-15 00:35:40 -0300

    meu primeiro commit: configando o server da aplicação usando gulp

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+
+gulp.task('default', ['webserver', 'watch']);
+
+/*Serves the aplication*/
+gulp.task('webserver', function () {
+    connect.server({
+        livereload: true
+    });
+
+});
+
+/*gulp saas for scss*/
+gulp.task('sass', function (done) {
+    gulp.src(['./assests/scss/*.scss'])
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./assets/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        // .pipe(gulp.dest('./www/assets/css/'))
+        .pipe(gulp.dest('./assets/css/'))
+        .on('end', done);
+});
+
+//html files
+gulp.task('html', function () {
+    gulp.src('*.html')
+        .pipe(connect.reload());
+})
+gulp.task('watch', function () {
+    gulp.watch(['*.html'], ['html']);
+    gulp.watch(['./assests/scss/*.scss'], ['sass']);
+})
